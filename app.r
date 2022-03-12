5ee8052f182abec16f1dd58aca89979ebb7e2b5f
Ameesha Saxena <ameeshasaxena23@gmail.com>
2022-03-12 13:59:22 -0600

    added difference plot to bar chart and map TODO Table

--- app.r
+++ app.r
@@ -11,6 +11,7 @@ library(plyr)
 library(readr)
 library(leaflet)
 library(leaflet.providers)
+library(viridis)
 
 #read in the data for stations
 temp = list.files(pattern="*.csv", full.name = T)
@@ -45,19 +46,19 @@ years <- c(2001:2021)
 marker_radius <- function(num_rides) {
   radius = 0
   if (num_rides < 1000.0){
-    radius = 4
-  }
-  else if (num_rides < 2000.0){
     radius = 6
   }
-  else if (num_rides < 2500.0){
+  else if (num_rides < 2000.0){
     radius = 8
   }
-  else if(num_rides < 4000.0){
+  else if (num_rides < 2500.0){
     radius = 10
   }
-  else{
+  else if(num_rides < 4000.0){
     radius = 12
+  }
+  else{
+    radius = 14
   }
   return (radius)
 }
@@ -118,34 +119,41 @@ ui <- dashboardPage(
                        align = "center",
                        fluidRow(
                          style = "padding-left:20px",
-                         HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"),
+                         HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"),
+                         h3("View station rides"),
                          dateInput("dataDate",
-                                   h3("Enter Date"),
+                                   h4("Select a date"),
                                    value = "2021-08-23",
                                    min = "2001-01-01",
                                    max = "2021-11-30"
                                    ),
-                         actionButton("prevButton", "Previous"),
-                         actionButton("nextButton", "Next"),
-                         radioButtons("sortData", h3("Sort Bars"),
+                         actionButton("prevButton", "Previous date"),
+                         actionButton("nextButton", "Next date"),
+                         radioButtons("sortData", h4("Sort Bars"),
                                       choices = list("Alphabetically" = 0, 
                                                      "Ascending" = 1),selected = 0),
+                         checkboxInput("difference", "View Difference in rides for two dates", value = FALSE),
+                         dateInput("dataDate2",
+                                   h4("Select second date to view difference in rides"),
+                                   value = "2021-08-24",
+                                   min = "2001-01-01",
+                                   max = "2021-11-30"
+                         ),
+                         HTML("<br><br>"),
                          #selectInput("stationname", "Select a Station", station_name)
                          h3("Select a station and year to view visualizations"),
-                         radioButtons("plotOrTable", h3("Plots or Tables?"),
-                                      choices = list("Plots" = 0, 
-                                                     "Tables" = 1),selected = 0, inline="T"),
                          selectizeInput('stationname', label = NULL, choices = NULL, options = list(placeholder = 'Select a Station Name')),
                          selectInput("year", NULL, years, selected = "2021"),
-                         checkboxInput("difference", "View Difference in rides for the years", value = FALSE),
-                         selectInput("year2", "select year to view difference", years, selected = "2021"),
+                         radioButtons("plotOrTable", h3("Plots or Tables?"),
+                                      choices = list("Plots" = 0, 
+                                                     "Tables" = 1),selected = 0, inline="T")
                        ),
                 ),
                 column(8,
                        fluidRow(
                          #box(title = "Entries at L Stations on August 23, 2021", solidHeader = TRUE, status = "primary", width = 12,
                          box(title = textOutput("barChart"), solidHeader = TRUE, status = "primary", width = 12,
-                             plotOutput("initialChart", height = 600)
+                             plotOutput("initialChart", height = 700)
                          )
                        ),
                        fluidRow(
@@ -164,7 +172,7 @@ ui <- dashboardPage(
                                   fluidRow(
                                     style = "padding-left:20px",
                                     box(title = paste("Rides per Day"), solidHeader = TRUE, status = "primary", width = 40,
-                                        plotOutput("AllDays", height = 300)
+                                        plotOutput("AllDays", height = 600)
                                     )
                                   )
                                   ),
@@ -172,7 +180,7 @@ ui <- dashboardPage(
                                   fluidRow(
                                     style = "padding-left:20px",
                                     box(title = paste("Rides per Month"), solidHeader = TRUE, status = "primary", width = 40,
-                                        plotOutput("MonthlyData", height = 300)
+                                        plotOutput("MonthlyData", height = 600)
                                     )
                                   )
                                   ),
@@ -180,7 +188,7 @@ ui <- dashboardPage(
                                   fluidRow(
                                     style = "padding-left:20px",
                                     box(title = paste("Rides per Weekday"), solidHeader = TRUE, status = "primary", width = 40,
-                                        plotOutput("WeeklyData", height = 300)
+                                        plotOutput("WeeklyData", height = 600)
                                     )
                                   )
                                   )
@@ -217,7 +225,7 @@ ui <- dashboardPage(
                          style = "padding-left:20px",
                          #box(title = "Map of L Stations on August 23, 2021", solidHeader = TRUE, status = "primary", width = 12,
                          box(title = textOutput("map"), solidHeader = TRUE, status = "primary", width = 12,
-                             leafletOutput("leaflet", height = 1200)
+                             leafletOutput("leaflet", height = 1400)
                          )
                        ),
                        fluidRow(
@@ -247,6 +255,10 @@ server <- function(input, output, session) {
     as_date(input$dataDate)
   })
   
+  dateBarChart2 <- reactive({
+    as_date(input$dataDate2)
+  })
+  
   dataSort <- reactive({
     input$sortData
   })
@@ -259,10 +271,12 @@ server <- function(input, output, session) {
     as.numeric(input$year2)
   })
   
-  
-  
   stationData <- reactive({
     station_data <- subset(ridership_data, ridership_data$stationname == input$stationname)
+  })
+  
+  dateDifference<- reactive({
+    input$difference
   })
   
   #stationName selection
@@ -318,12 +332,19 @@ server <- function(input, output, session) {
     return(paste("Stations and Entries on ", date, " - ", weekdays(date)))
   })
   
+  
   #Total entries at all L stations for Date
   output$initialChart <- renderPlot({
     # Add up entries for each Station on Date
-    ridershipAug23 <- subset(ridership_data, ridership_data$newDate == dateBarChart())
-    byStation <- setNames(aggregate(ridershipAug23$rides, by=list(ridershipAug23$stationname), sum), c("Station", "Entries"))
-
+    ridershipDate <- subset(ridership_data, ridership_data$newDate == dateBarChart())
+    ridershipDate2 <- subset(ridership_data, ridership_data$newDate == dateBarChart2())
+    
+    if(dateDifference()){
+      ridershipDate$rides <- ridershipDate$rides - ridershipDate2$rides
+    }
+    
+    byStation <- setNames(aggregate(ridershipDate$rides, by=list(ridershipDate$stationname), sum), c("Station", "Entries"))
+    
     if(dataSort() == 0){
       m <- ggplot(byStation, aes(x=Station, y=Entries)) 
     }
@@ -332,13 +353,18 @@ server <- function(input, output, session) {
     }
     
     m <- m + geom_bar(stat="identity", width=0.7, fill="#33647A") +
-      labs(x=paste("Station Name"), y="Total Entries") +
+      #scale_fill_manual(values = c("(-Inf, 0]" = "#601e1e", "[1, Inf)" = "#153e51")) +
+      #scale_fill_gradient2(midpoint = 0, low = '#082b3a', high = '#490f0f') +
       theme_bw() +
+      labs(x=paste("Station Name"), y="Total Entries") +
       theme(text = element_text(family = "sans", face = "bold")) +
       theme(axis.text.x = element_text(angle = 70, hjust=1))
+      
     m
-    
+    #fill="#33647A"
   })
+  
+  
   
   #bad code - can't find how to add a list of markers
   output$leaflet <- renderLeaflet({
@@ -347,33 +373,44 @@ server <- function(input, output, session) {
     m <- addTiles(m)
     if(input$mapView == 2){
       m <- addProviderTiles(m, provider = "Esri.WorldImagery")
-      marker_color = "red"
     }
     else if(input$mapView == 3){
       m <- addProviderTiles(m, provider = "OpenTopoMap")
     }
-      
+    
     #TODO add the lat and long for the last four stations as (lat, long)
     #add legend for sizing
     DateSub <- subset(ridership_data, newDate == dateBarChart())
-    DateSubSums <- aggregate(DateSub$rides, by=list(station_id=DateSub$station_id), FUN=sum)
+    DateSub2 <- subset(ridership_data,  newDate == dateBarChart2())
     
+    
+    if(dateDifference()){
+      DateSub$rides <- DateSub$rides - DateSub2$rides
+    }
+    
+    DateSubSums <- aggregate(DateSub$rides, by=list(station_id=DateSub$station_id), FUN=sum)
+
     #Pick 3 backgrounds from http://leaflet-extras.github.io/leaflet-providers/preview/
     #m <- addProviderTiles(m, provider = "Esri.WorldImagery") #Thunderforest.Transport
     station_ids = strsplit(temp, "./")
     for (i in 1:nrow(DateSubSums)){
+      marker_color = "blue"
       a <- subset(stopData, MAP_ID == DateSubSums[i, "station_id"])
       string <- a$Location[1]
       mat = matrix(scan(text = gsub("[()]", "", string), sep = ","), 
                    ncol = 2, byrow = TRUE, dimnames = list(NULL, c("Lat", "Long")))
       #m <- addMarkers(m, lng=mat[1,2], lat=mat[1,1], popup=a$STOP_NAME[1])
+      if(DateSubSums[i, "x"] < 0){
+        marker_color = "red"
+      }
       m <- addCircleMarkers(m, lng=mat[1,2], lat=mat[1,1], #popup=c(a$STOP_NAME[1], DateSubSums[i, "x"]),
                             popup = paste(sep="",
                                           "<b>", a$STOP_NAME[1],"</b>","<br/>",
                                           "<b>", DateSubSums[i, "x"], "</b>"
                             ), 
-                            radius = marker_radius(DateSubSums[i, "x"]), color = marker_color)
+                            radius = marker_radius(abs(DateSubSums[i, "x"])), color = marker_color, fill = FALSE)
     }
+    
     m <- addLegend(m,
                    "bottomright", 
                    colors = paste0(c("blue", "blue", "blue", "blue", "blue"), "; border-radius:", c(40, 60, 80, 100, 120), "px; width:", c(10, 15, 20, 25, 30), "px; height:", c(10, 15, 20, 25, 30), "px"),
@@ -405,9 +442,9 @@ server <- function(input, output, session) {
     col <- c("#3e6a7f", "#749aa6", "#3e6a7f", "#749aa6", "#3e6a7f", "#749aa6", "#3e6a7f", "#749aa6", "#3e6a7f", "#749aa6", "#3e6a7f", "#749aa6")
     YearSub <- subset(stationData(), year == inputYear())
     YearSubSums <- setNames(aggregate(YearSub$rides, by=list(YearSub$newDate), FUN=sum), c("date", "rides"))
-    ggplot(YearSubSums, aes(x = date, y = rides/1000, fill = month(date,abbr = TRUE, label = TRUE))) + 
+    ggplot(YearSubSums, aes(x = date, y = rides, fill = month(date,abbr = TRUE, label = TRUE))) + 
       geom_bar(stat = "identity", show.legend = FALSE) +
-      labs(x = "Date", y ="Rides (in thousands)") + 
+      labs(x = "Date", y ="Rides") + 
       theme_bw() +
       #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
       theme(text = element_text(family = "sans", face = "bold")) +
@@ -435,6 +472,7 @@ server <- function(input, output, session) {
       theme(plot.title = element_text(hjust = 0.5, size=20), axis.title=element_text(size=12))  
   })
   
+  
   # Per-Station Data Tables
   
   output$TableAllDays <- DT::renderDataTable(
@@ -443,7 +481,7 @@ server <- function(input, output, session) {
       YearSubSums <- setNames(aggregate(YearSub$rides, by=list(YearSub$newDate), FUN=sum), c("Date", "Rides"))
       YearSubSums
     }, 
-    options = list(searching = FALSE, pageLength = 7, lengthChange = FALSE, order = list(list(0, 'asc'))
+    options = list(searching = FALSE, pageLength = 14, lengthChange = FALSE, order = list(list(0, 'asc'))
     ), rownames = FALSE 
     )
   )
@@ -454,7 +492,7 @@ server <- function(input, output, session) {
       YearSubSums <- setNames(aggregate(YearSub$rides, by=list(YearSub$month), FUN=sum), c("Month", "Rides"))
       YearSubSums
     }, 
-    options = list(searching = FALSE, pageLength = 7, lengthChange = FALSE, order = list(list(0, 'asc'))
+    options = list(searching = FALSE, pageLength = 14, lengthChange = FALSE, order = list(list(0, 'asc'))
     ), rownames = FALSE 
     )
   )
@@ -465,7 +503,7 @@ server <- function(input, output, session) {
       YearSubSums <- setNames(aggregate(YearSub$rides, by=list(YearSub$wday), FUN=sum), c("wday", "rides"))
       YearSubSums
     }, 
-    options = list(searching = FALSE, pageLength = 7, lengthChange = FALSE, order = list(list(0, 'asc'))
+    options = list(searching = FALSE, pageLength = 14, lengthChange = FALSE, order = list(list(0, 'asc'))
     ), rownames = FALSE 
     )
   )
